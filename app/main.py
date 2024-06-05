4bf03423be6a0d2e337cb7ced127374608ffa5ca
Nathan Beam <nathan.w.beam@gmail.com>
2024-06-04 21:13:36 -0700

    WIP Firebase Auth

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI, HTTPException, BackgroundTasks, status, Response
+from fastapi import FastAPI, HTTPException, BackgroundTasks, status, Response, Depends
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import PlainTextResponse
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from typing import List
 from app.models import (
     Bookmark,
@@ -10,25 +11,16 @@ from app.models import (
     DocumentDisplay,
     HTTPError,
     SearchPayload,
-    SearchResults,
-    SubTopicDisplay,
-    TreeNode,
 )
 import logging
 import sys
+import re
 import app.app_logic as app_logic
-import app.subtopics_util as subtopics_util
-import app.html_parser as html_parser
-import app.getters as getter
-from app.search_handler import SearchHandler
-from app.prompt_models import RAGPrompt
-
-search = SearchHandler()
-
+from firebase_admin import auth, initialize_app
 
 logging.basicConfig(
     stream=sys.stdout,
-    format="%(asctime)s - %(funcName)s:%(lineno)d - %(message)s",
+    format="%(asctime)s - %(message)s",
     level=logging.DEBUG,
     datefmt="%Y-%m-%d %H:%M:%S",
 )
@@ -45,8 +37,10 @@ app.add_middleware(
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
-    expose_headers=["icognitoin-answer-type"],
 )
+
+# Initialize Firebase
+_ = initialize_app()
 
 
 @app.get("/")
@@ -60,7 +54,8 @@ async def ping():
         bm = app_logic.test_db_connection()
     except Exception as e:
         logging.error(e)
-        raise HTTPException(status_code=500, detail="Database connection failed")
+        raise HTTPException(
+            status_code=500, detail="Database connection failed")
 
     return {"Message": "Service is up and running"}
 
@@ -70,6 +65,25 @@ async def validation_exception_handler(request, exc):
     logging.error(request)
     logging.error(exc)
     return PlainTextResponse(str(request), status_code=400)
+
+
+def get_current_user(
+        _: Response,
+        credential: HTTPAuthorizationCredentials = Depends(
+            HTTPBearer(auto_error=False))
+):
+    if credential is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized")
+    try:
+        print("Yeah")
+        decoded_user = auth.verify_id_token(credential.credentials)
+        print("User ID: ", decoded_user["uid"])
+        return decoded_user
+    except Exception as e:
+        print("Error updated: ", e)
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized")
 
 
 @app.post(
@@ -83,7 +97,7 @@ async def validation_exception_handler(request, exc):
     },
 )
 async def create_bookmark(
-    payload: PagePayload, background_tasks: BackgroundTasks, response: Response
+    payload: PagePayload, background_tasks: BackgroundTasks, response: Response, user=Depends(get_current_user)
 ):
 
     logging.info(f"Icognition bookmark endpoint called on {payload.url}")
@@ -96,11 +110,11 @@ async def create_bookmark(
         )
 
     # Check if payload.url is not the root URL of a website
-    if html_parser.unsupported_page_url(payload.url):
+    if re.match(r"^https?://[^/]+/$", payload.url):
         logging.warn(f"Invalid URL provided: {payload.url}")
         raise HTTPException(
             status_code=400,
-            detail="I am sorry, I can't analyze home or search pages",
+            detail="I am sorry, I can't analyze home pages",
         )
 
     page = app_logic.create_page(payload)
@@ -112,8 +126,8 @@ async def create_bookmark(
             detail="Hmm, I wasn't able to find information on this page. I sent a message to our engineers",
         )
 
-    ## Check in bookmark already exists
-    bookmark = getter.get_bookmark_by_url(payload.user_id, page.clean_url)
+    # Check in bookmark already exists
+    bookmark = app_logic.get_bookmark_by_url(page.clean_url)
     if bookmark is not None:
         logging.info(f"Bookmark already exists for {page.clean_url}")
         response.status_code = status.HTTP_201_CREATED
@@ -122,7 +136,7 @@ async def create_bookmark(
         logging.info(f"Page object created for {page.clean_url}")
         bookmark = app_logic.create_bookmark(page, payload.user_id)
         logging.info(f"Bookmark created for {bookmark.url}")
-        background_tasks.add_task(generate_document, bookmark)
+        background_tasks.add_task(generate_document, bookmark.document_id)
         response.status_code = status.HTTP_201_CREATED
         return bookmark
 
@@ -148,7 +162,8 @@ async def post_regenerate_document(
     # and the bookmark will be used to get the new document
     new_doc = app_logic.clone_document(old_doc)
     background_tasks.add_task(regenerate_document, new_doc)
-    bookmark = app_logic.reassociate_bookmark_with_document(old_doc.id, new_doc.id)
+    bookmark = app_logic.reassociate_bookmark_with_document(
+        old_doc.id, new_doc.id)
 
     if bookmark is None:
         raise HTTPException(status_code=404, detail="Bookmark not found")
@@ -156,50 +171,42 @@ async def post_regenerate_document(
     return bookmark
 
 
-## Background task to generate summaries from LLM
-async def generate_document(bookmark: Bookmark):
-    document = getter.get_document_by_id(bookmark.document_id)
+# Background task to generate summaries from LLM
+async def generate_document(document_id):
+    document = app_logic.get_document_by_id(document_id)
 
     if document.status in ["Pending", "Done", "Failure"]:
-        logging.info(f"Background task for document ID: {bookmark.document_id}")
-        await app_logic.extract_info_from_doc(document)
-        await app_logic.generate_embeddings(bookmark.user_id)
-        await subtopics_util.subtopics_factory(bookmark.user_id)
-        logging.info(f"Background task for document ID: {bookmark.document_id} completed")
+        logging.info(f"Background task for document ID: {document_id}")
+        document = await app_logic.extract_info_from_doc(document)
+        logging.info(f"Background task for document ID: {
+                     document_id} completed")
 
 
-## Background task to regenerate summaries from LLM if not already exists
+# Background task to regenerate summaries from LLM if not already exists
 async def regenerate_document(doc: Document):
 
     if doc.status in ["Pending", "Done", "Failure"]:
 
         new_doc = await app_logic.extract_info_from_doc(doc)
         if new_doc:
-            logging.info(f"Background task for document ID: {new_doc.id} completed")
+            logging.info(f"Background task for document ID: {
+                         new_doc.id} completed")
         else:
             logging.error(
-                f"Background document regenaring for document ID: {doc.id} failed"
+                f"Background document regenaring for document ID: {
+                    doc.id} failed"
             )
 
 
-@app.get("/bookmarks/user/{user_id}", response_model=List[Bookmark], status_code=200)
+@app.get("/bookmark/user/{user_id}", response_model=List[Bookmark], status_code=200)
 async def get_bookmarks_by_user_id(user_id: str):
-    bookmarks = getter.get_bookmarks_by_user_id(user_id)
+    bookmarks = app_logic.get_bookmarks_by_user_id(user_id)
 
     if bookmarks is None:
         raise HTTPException(status_code=404, detail="Bookmarks not found")
 
     logging.info(f"Icognition return {len(bookmarks)} bookmarks")
     return bookmarks
-
-@app.post("/bookmark/user", response_model=Bookmark, status_code=200)
-async def get_bookmarks_by_user_id(payload: PagePayload):
-    bookmark = getter.get_bookmark_by_url(payload.user_id, payload.url)
-
-    if bookmark is None:
-        raise HTTPException(status_code=404, detail="Bookmark not found")
-
-    return bookmark
 
 
 @app.get(
@@ -208,19 +215,17 @@ async def get_bookmarks_by_user_id(payload: PagePayload):
     status_code=200,
 )
 async def get_documents_plus_by_user_id(user_id: str):
-    bookmarks = getter.get_bookmarks_by_user_id(user_id)
+    bookmarks = app_logic.get_bookmarks_by_user_id(user_id)
 
     if bookmarks is None:
         raise HTTPException(status_code=404, detail="Bookmarks not found")
 
     results = []
     for bookmark in bookmarks:
-        document = getter.get_document_by_id(bookmark.document_id)
-        if document:
-            display = document.to_display()
-        else:
-            logging.warn(f"Document not found for bookmark {bookmark.id}") 
+        document = app_logic.get_document_by_id(bookmark.document_id)
+        entities = app_logic.get_entities_by_document_id(document.id)
 
+        display = DocumentDisplay.from_orm(document, entities=entities)
 
         results.append(display)
 
@@ -230,7 +235,7 @@ async def get_documents_plus_by_user_id(user_id: str):
 
 @app.get("/bookmark", response_model=Bookmark, status_code=200)
 async def get_bookmark_by_url(url: str):
-    bookmark = getter.get_bookmark_by_url(url)
+    bookmark = app_logic.get_bookmark_by_url(url)
 
     if bookmark is None:
         raise HTTPException(status_code=404, detail="Bookmark not found")
@@ -242,7 +247,7 @@ async def get_bookmark_by_url(url: str):
 @app.get("/bookmark/{id}/document")
 async def get_bookmark_document(id: int, response: Response):
     logging.info(f"Icognition bookmark document endpoint called on {id}")
-    document = getter.get_document_by_bookmark_id(id)
+    document = app_logic.get_document_by_bookmark_id(id)
 
     if document is None:
         raise HTTPException(status_code=404, detail="Document not found")
@@ -256,19 +261,12 @@ async def get_bookmark_document(id: int, response: Response):
         return document
 
 
-@app.get("/document_plus/{bookmark_id}", responses={
-        404: {
-            "model": HTTPError,
-            "description": "Returning document error",
-        },
-        206: {"model": None, "description": "Document is being processed"},
-        200: {"model": DocumentDisplay, "description": "Document is ready"},
-    })
+@app.get("/document_plus/{bookmark_id}", response_model=DocumentDisplay)
 async def get_document_plus(bookmark_id: int, response: Response, background_tasks: BackgroundTasks):
     """get document with entities and concepts"""
 
     logging.info(f"Document plus -> endpoint called on bookmark {bookmark_id}")
-    document = getter.get_document_by_bookmark_id(bookmark_id)
+    document = app_logic.get_document_by_bookmark_id(bookmark_id)
 
     if document is None:
         raise HTTPException(status_code=404, detail="Document not found")
@@ -277,26 +275,32 @@ async def get_document_plus(bookmark_id: int, response: Response, background_tas
     if document.status in ["Processing", "Pending"]:
         response.status_code = status.HTTP_206_PARTIAL_CONTENT
         logging.info(
-            f"Document plus -> endpoint called on document status {document.status}"
+            f"Document plus -> endpoint called on document status {
+                document.status}"
         )
-        return None
+        return DocumentDisplay.from_orm(document)
     elif document.status == "Done":
-        
+        entities = app_logic.get_entities_by_document_id(document.id)
+
         response.status_code = status.HTTP_200_OK
         logging.info(
-            f"Document plus -> endpoint called on document status {document.status}"
+            f"Document plus -> endpoint called on document status {
+                document.status}"
         )
 
-        return document.to_display()
+        # Generate embeddings for the document
+        background_tasks.add_task(generate_embedding)
+
+        return DocumentDisplay.from_orm(document, entities=entities)
     else:
         response.status_code = status.HTTP_404_NOT_FOUND
-        return document.to_display()
+        return DocumentDisplay.from_orm(document)
 
 
 @app.get("/document/{id}")
 async def get_document(id: int, response: Response):
     logging.info(f"Icognition document endpoint called on {id}")
-    document = getter.get_document_by_id(id)
+    document = app_logic.get_document_by_id(id)
 
     if document is None:
         raise HTTPException(status_code=404, detail="Document not found")
@@ -310,8 +314,20 @@ async def get_document(id: int, response: Response):
         return document
 
 
+@app.get("/document/{id}/entities")
+async def get_entities(id: int, response: Response):
+    logging.info(f"Icognition document entities endpoint called on {id}")
+    entities = app_logic.get_entities_by_document_id(id)
 
-@app.delete("/bookmark/{id}", status_code=204)
+    if entities is None:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        raise HTTPException(status_code=404, detail="Entities not found")
+    else:
+        response.status_code = status.HTTP_200_OK
+        return entities
+
+
+@app.delete("/bookmark/{id}/document", status_code=204)
 async def delete_bookmark(id: int) -> None:
     logging.info(f"Delete bookmark and associated records for id: {id}")
     app_logic.delete_bookmark_and_associate_records(id)
@@ -323,59 +339,32 @@ async def delete_document(id: int) -> None:
     app_logic.delete_document_and_associate_records(id)
 
 
-@app.get("/subtopics/{user_id}", response_model=List[SubTopicDisplay], status_code=200)
-async def get_user_subtopics(user_id: str):
-    try:
-        subtopics = getter.get_subtopics_display(user_id = user_id)
-        return subtopics
-    except Exception as e:
-        logging.error(e)
-        raise HTTPException(status_code=404, detail="Subtopics not found")
+@app.get("/entities/{user_id}", response_model=List, status_code=200)
+async def post_entities(user_id: str):
+    logging.info(f"Post entities tree")
+    entities = app_logic.get_entities_by_user_id(user_id)
+    return entities
 
 
-@app.get("/subtopics_node/{user_id}", response_model=List[TreeNode], status_code=200)
-async def get_user_subtopics_node(user_id: str):
-    try:
-        subtopics_nodes = getter.get_subtopics_nodes_by_user(user_id)
-        return subtopics_nodes
-    except Exception as e:
-        logging.error(e)
-        raise HTTPException(status_code=404, detail="Subtopics not found")
-
-@app.get("/subtopics_name_regenerate/{user_id}", status_code=200)
-async def regenerate_user_subtopics(user_id: str, background_tasks: BackgroundTasks):
-    try:
-        background_tasks.add_task(subtopics_util.rename_subtopics, user_id)
-        return {"Message": "Subtopics regeneration submitted"}
-    except Exception as e:
-        logging.error(e)
-        raise HTTPException(status_code=404, detail="Subtopics regeneration failed")
+@app.get("/entities_tree/{user_id}", response_model=List, status_code=200)
+async def post_entities_tree(user_id: str):
+    logging.info(f"Post entities tree")
+    entities = app_logic.get_entities_tree_by_user_id(user_id)
+    return entities
 
 
-@app.post("/search", status_code=200, response_model=SearchResults)
-async def search_documents(search_payload: SearchPayload, response: Response):
+@app.post("/search", response_model=List[DocumentDisplay], status_code=200)
+async def search_documents(search_payload: SearchPayload):
     logging.info(f"Search documents")
-    
-    try:
-        results = await search(search_payload.user_id, search_payload.query)
-    except Exception as e:
-        logging.error(e)
-        raise HTTPException(status_code=404, detail="Search failed")
-    
-    if results.failure:
-        return results
 
-    if len(results.documents_display) == 0:
-        raise HTTPException(status_code=404, detail="No results found")
-
-    if results.rag_answer:
-        response.headers["icognitoin-answer-type"] = "RAGAnswer"
-        return results
+    if (search_payload.query == None):
+        documents = app_logic.search_documents(
+            search_payload.user_id, search_payload.query)
     else:
-        response.headers["icognitoin-answer-type"] = "DocumentDisplay"
-        return results
-        
+        documents = app_logic.search_embeddings(
+            search_payload.user_id, search_payload.query)
 
+    return documents
 
 
 @app.get("/generate_embedding", status_code=200)
@@ -385,27 +374,5 @@ async def generate_embedding():
         return {"Message": "Embedding generation completed"}
     except Exception as e:
         logging.error(e)
-        raise HTTPException(status_code=500, detail="Embedding generation failed")
-
-@app.delete("/subtopics/{user_id}", status_code=204)
-def delete_user_id_subtopics(user_id: str):
-    logging.info(f"Delete subtopics for user_id: {user_id}")
-    subtopics_util.delete_user_id_subtopics(user_id) 
-
-@app.get("/regenerate/subtopics/{user_id}", status_code=200)
-async def regenerate_subtopics(_user_id: str, background_tasks: BackgroundTasks):
-    try:
-        subtopics_util.delete_user_id_subtopics(_user_id)
-        background_tasks.add_task(subtopics_util.subtopics_factory, user_id = _user_id, force_run = True)
-    except Exception as e:
-        logging.error(e)
-        raise HTTPException(status_code=500, detail="Subtopics generation failed")
-    
-@app.get("/generate/subtopics/{user_id}", status_code=200)
-async def regenerate_subtopics(_user_id: str, background_tasks: BackgroundTasks):
-    try:
-        background_tasks.add_task(subtopics_util.subtopics_factory, 
-                                  user_id = _user_id, force_run = True)
-    except Exception as e:
-        logging.error(e)
-        raise HTTPException(status_code=500, detail="Subtopics generation failed")
+        raise HTTPException(
+            status_code=500, detail="Embedding generation failed")
