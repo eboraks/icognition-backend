eec249721992dd22dc3f0dff35f86236facdbd5c
Eliran Boraks <eboraks@gmail.com>
2025-12-03 19:53:44 -0500

    migration to sql cloud

--- backend/migrations/env.py
+++ backend/migrations/env.py
@@ -1,11 +1,20 @@
 from logging.config import fileConfig
 import os
 import sys
+from dotenv import load_dotenv
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
+
+load_dotenv()
+
+connection_string = os.getenv("DATABASE_URL")
+
+if not connection_string:
+    raise ValueError("DATABASE_URL is not set")
+
 
 # Add the project root to the Python path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
@@ -17,6 +26,7 @@ from app.models import *  # Import all models
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+config.set_main_option("sqlalchemy.url", connection_string)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
